9079cd5969c252c83e9fed43e9278ca9d5d618ce
devendran.jagadisan@gmail.com <devendran.jagadisan@gmail.com>
2013-05-22 08:12:58 +1000

    update on csv

--- write_to_csv.py
+++ write_to_csv.py
@@ -4,27 +4,24 @@ import csv
 
 class WriteCSV():
     
-    def createCSVFile(self,filename):
+    def createCSVFile(self,filename,data):
         
+   
         if os.path.exists(filename) is False:
             try:
-                _log = open(filename, 'w+')
-                return _log
+                record = open(filename, 'w+')
+                writer = csv.writer(record,delimiter=',',quoting=csv.QUOTE_ALL)
+                writer.writerow([data[0],data[1],data[2],data[3],data[4],data[5]])
+               
             except IOError,e:
                 print "File Error" %e
                 raise SystemExit
         else:
-            return False
-        
-    def WriteCSVFile(self,filename,data): 
-        
-        '''
-        print filename
-        print data[0]
-        record_ = open(filename, 'wb')
-        writer = csv.writer(record_,delimiter=',',quoting=csv.QUOTE_ALL)
-        writer.writerow([data[0],data[1],data[2],data[3],data[4],data[5]])
-        '''
+            with open(filename,'a') as w:
+                writer = csv.writer(w,delimiter=',',quoting=csv.QUOTE_ALL)
+                writer.writerow([data[0],data[1],data[2],data[3],data[4],data[5]])
+    
+
         
 
         
